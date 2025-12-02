4971704e8fa700a90666cd8e262b69b56a9697ed
Bruno Carrodeguas <brunocarrodeguas@gmail.com>
2025-12-02 12:39:10 +0100

    V 1.0

--- src/main/java/com/liceolapaz/bcd/judokascompeticion/Application.java
+++ src/main/java/com/liceolapaz/bcd/judokascompeticion/Application.java
@@ -3,6 +3,7 @@ package com.liceolapaz.bcd.judokascompeticion;
 import com.liceolapaz.bcd.judokascompeticion.navigation.AppView;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,14 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource(AppView.LOGIN.getFxmlFile()));
         Scene scene = new Scene(fxmlLoader.load(), 500, 350);
+        try {
+
+            Image icon = new Image(getClass().getResourceAsStream("/img/icono.png"));
+            stage.getIcons().add(icon);
+        } catch (Exception e) {
+            System.err.println("Error al cargar el icono: /img/icono.png");
+            e.printStackTrace();
+        }
         stage.setTitle("Inicio de sesión");
         stage.setResizable(false);
         stage.setScene(scene);
